66e6fd0817899e3791a96aa63772f682435c3cf6
Sri Kadimisetty <kadimisetty@users.noreply.github.com>
2019-07-25 05:30:27 +0530

    test getts and about

---
+++ tests/test_getters.py
@@ -0,0 +1,18 @@
+import unittest
+
+from action_heroes.getters import get_about, get_config_as_dict
+
+
+class TestGetAbout(unittest.TestCase):
+    def setUp(self):
+        self.about = get_about()
+
+    def test_get_about_returns_dict(self):
+        self.assertIsInstance(self.about, dict)
+
+    def test_get_about_returns_nonempty(self):
+        self.assertGreater(len(self.about), 0)
+
+
+class TestGetConfigAsGet(unittest.TestCase):
+    pass
