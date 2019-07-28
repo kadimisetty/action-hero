3af75771bac1339d90e82a11cff0f03c551e4c46
Sri Kadimisetty <kadimisetty@users.noreply.github.com>
2019-07-28 09:46:03 +0530

    add valid ipv4 and ipv6 addresses and tests

--- action_heroes/net.py
+++ action_heroes/net.py
@@ -1,25 +1,69 @@
 from argparse import Action
 
+from action_heroes.net_utils import (
+    is_valid_ip_address,
+    is_valid_ipv4_address,
+    is_valid_ipv6_address,
+    is_reachable_url,
+    status_code_from_response_to_request_url,
+)
+
 
 class IPIsValidIPv4AddressAction(Action):
     """Check if IP is valid ipv4 address"""
 
-    def __call__(self):
-        raise NotImplementedError
+    def __call__(self, parser, namespace, values, option_string=None):
+        if isinstance(values, list):
+            # Check list of ips are all valid ipv4 addresses
+            if False in [is_valid_ipv4_address(path) for path in values]:
+                raise ValueError(
+                    "ips has atleast one ip that is not a valid ipv4 address"
+                )
+        else:
+            # Check ip is valid ip4 address
+            path = values
+            if not is_valid_ipv4_address(path):
+                raise ValueError("ip is not a valid ipv4 address")
+
+        setattr(namespace, self.dest, values)
 
 
-class IPIsValidIPV6Action(Action):
+class IPIsValidIPv6AddressAction(Action):
     """Check if IP is valid ipv6 address"""
 
-    def __call__(self):
-        raise NotImplementedError
+    def __call__(self, parser, namespace, values, option_string=None):
+        if isinstance(values, list):
+            # Check list of ips are all valid ipv6 addresses
+            if False in [is_valid_ipv6_address(path) for path in values]:
+                raise ValueError(
+                    "ips has atleast one ip that is not a valid ipv6 address"
+                )
+        else:
+            # Check ip is valid ip6 address
+            path = values
+            if not is_valid_ipv6_address(path):
+                raise ValueError("ip is not a valid ipv6 address")
+
+        setattr(namespace, self.dest, values)
 
 
-class IPIsValidIPAction(Action):
-    """Check if URL is valid"""
+class IPIsValidIPAddressAction(Action):
+    """Check if ip is valid ipv4 or ipv6 address"""
 
-    def __call__(self):
-        raise NotImplementedError
+    def __call__(self, parser, namespace, values, option_string=None):
+        if isinstance(values, list):
+            # Check list of ips are all valid ipv4/ipv6 addresses
+            if False in [is_valid_ipv6_address(path) for path in values]:
+                raise ValueError(
+                    "ips has atleast one ip that is not a valid ip address"
+                )
+        else:
+            # Check ip is valid ipv4/ipv6 address
+            path = values
+            if not is_valid_ipv6_address(path):
+                raise ValueError("ip is not a valid ip address")
+
+        setattr(namespace, self.dest, values)
 
 
 class URLIsReachableAction(Action):
--- tests/test_net.py
+++ tests/test_net.py
@@ -1,1 +1,106 @@
 import unittest
+from argparse import ArgumentParser
+
+from action_heroes.net import (
+    IPIsValidIPv4AddressAction,
+    IPIsValidIPv6AddressAction,
+    IPIsValidIPAddressAction,
+)
+
+from action_heroes.net_utils import (
+    is_valid_ip_address,
+    is_valid_ipv4_address,
+    is_valid_ipv6_address,
+    is_reachable_url,
+    status_code_from_response_to_request_url,
+)
+
+
+class ParserEnclosedTestCase(unittest.TestCase):
+    def setUp(self):
+        """Setup new parser"""
+        self.parser = ArgumentParser()
+
+
+class TestIPIsValidIPv4AddressAction(ParserEnclosedTestCase):
+    def test_on_valid_ipv4_address(self):
+        self.parser.add_argument("--ip", action=IPIsValidIPv4AddressAction)
+        # Parse without raising any errors
+        self.parser.parse_args(["--ip", "192.168.0.2"])
+
+    def test_on_invalid_ipv4_address(self):
+        self.parser.add_argument("--ip", action=IPIsValidIPv4AddressAction)
+        # Asser error raised on parse
+        with self.assertRaises(ValueError):
+            self.parser.parse_args(["--ip", "500.168.0.1"])
+
+    def test_on_valid_ipv4_address_list(self):
+        self.parser.add_argument(
+            "--ip", nargs="+", action=IPIsValidIPv4AddressAction
+        )
+        ips = ["192.168.0.2"]
+        # Parse without raising any errors
+        self.parser.parse_args(["--ip", *ips])
+
+    def test_on_invalid_ipv4_address_list(self):
+        self.parser.add_argument(
+            "--ip", nargs="+", action=IPIsValidIPv4AddressAction
+        )
+        ips = ["500.168.0.1", "2001:db8:0:1"]
+        # Asser error raised on parse
+        with self.assertRaises(ValueError):
+            self.parser.parse_args(["--ip", *ips])
+
+    def test_on_mixed_valid_and_invalid_ipv4_addresses_list(self):
+        self.parser.add_argument(
+            "--ip", nargs="+", action=IPIsValidIPv4AddressAction
+        )
+        valid = ["192.168.0.2"]
+        invalid = ["500.168.0.1"]
+        # Asser error raised on parse
+        with self.assertRaises(ValueError):
+            self.parser.parse_args(["--ip", *valid, *invalid])
+
+
+class TestIPIsValidIPV6Action(ParserEnclosedTestCase):
+    def test_on_valid_ipv6_address(self):
+        self.parser.add_argument("--ip", action=IPIsValidIPv6AddressAction)
+        # Parse without raising any errors
+        self.parser.parse_args(["--ip", "FE80::0202:B3FF:FE1E:8329"])
+
+    def test_on_invalid_ipv6_address(self):
+        self.parser.add_argument("--ip", action=IPIsValidIPv6AddressAction)
+        # Asser error raised on parse
+        with self.assertRaises(ValueError):
+            self.parser.parse_args(["--ip", "10.168.0.1"])
+
+    def test_on_valid_ipv6_address_list(self):
+        self.parser.add_argument(
+            "--ip", nargs="+", action=IPIsValidIPv6AddressAction
+        )
+        ips = ["2001:db8:1::ab9:C0A8:102", "FE80::0202:B3FF:FE1E:8329"]
+        # Parse without raising any errors
+        self.parser.parse_args(["--ip", *ips])
+
+    def test_on_invalid_ipv6_address_list(self):
+        self.parser.add_argument(
+            "--ip", nargs="+", action=IPIsValidIPv6AddressAction
+        )
+        ips = ["122.168.0.1", "0.0.0.0"]
+        # Asser error raised on parse
+        with self.assertRaises(ValueError):
+            self.parser.parse_args(["--ip", *ips])
+
+    def test_on_mixed_valid_and_invalid_ipv6_addresses_list(self):
+        self.parser.add_argument(
+            "--ip", nargs="+", action=IPIsValidIPv6AddressAction
+        )
+        valid = ["192.168.0.2"]
+        invalid = ["122.168.0.1", "0.0.0.0"]
+        # Asser error raised on parse
+        with self.assertRaises(ValueError):
+            self.parser.parse_args(["--ip", *valid, *invalid])
+
+
+class TestIPIsValidIPAction(ParserEnclosedTestCase):
+    pass
