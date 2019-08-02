2ce7b5290e701d59f1714ce2dfc6be872ecb4a45
Sri Kadimisetty <kadimisetty@users.noreply.github.com>
2019-08-02 11:30:29 +0530

    limit py version to 3.6 and upwards

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
